79acb875c0ddd565a5179ebc36fffc98331b6c81
mahdi chekini <mchekini@checkconsulting.fr>
2023-07-14 13:52:47 +0200

    wip

--- src/main/java/com/example/staffmanagerapi/StaffManagerApiApplication.java
+++ src/main/java/com/example/staffmanagerapi/StaffManagerApiApplication.java
@@ -1,28 +1,11 @@
 package com.example.staffmanagerapi;
 
-import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.w3c.dom.ls.LSResourceResolver;
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.SAXException;
-
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.validation.Validator;
-import java.io.IOException;
 
 @SpringBootApplication
 public class StaffManagerApiApplication {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(StaffManagerApiApplication.class, args);
     }
