78423b53f8afe875156442a769f2f8ea6d6d4bb7
mahdi chekini <mchekini@checkconsulting.fr>
2023-07-14 13:36:25 +0200

    wip

--- src/test/java/com/example/staffmanagerapi/intergration/CustomerTest.java
+++ src/test/java/com/example/staffmanagerapi/intergration/CustomerTest.java
@@ -13,7 +13,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -53,6 +56,90 @@ class CustomerTest {
 
         // THEN
         assertEquals(201, response.getStatusCode().value());
+    }
+
+    @Test
+    void ItShouldDisplayAnErrorWhenPhoneNumberIsIncorrect() {
+
+        // GIVEN
+        Customer customer = Customer.builder()
+                .customerEmail("sephora@gmail.com")
+                .customerName("Sephora")
+                .customerAddress("france")
+                .customerPhone("012345")
+                .customerTvaNumber("FR01234567890")
+                .build();
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Customer> requestEntity = new HttpEntity<>(customer, headers);
+
+        // WHEN
+        ResponseEntity<Object> response = restTemplate.postForEntity("http://localhost:" + port + "/api/v1/customer", requestEntity, Object.class);
+
+        // THEN
+        Map<String, String> errors = (Map<String, String>) response.getBody();
+
+        assertEquals(400, response.getStatusCode().value());
+        assertTrue(errors.keySet().contains("customerPhone"));
+    }
+
+    @Test
+    void ItShouldDisplayAnErrorWhenTvaNumberIsIncorrect() {
+
+        // GIVEN
+        Customer customer = Customer.builder()
+                .customerEmail("sephora@gmail.com")
+                .customerName("Sephora")
+                .customerAddress("france")
+                .customerPhone("0123456789")
+                .customerTvaNumber("FR012345")
+                .build();
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Customer> requestEntity = new HttpEntity<>(customer, headers);
+
+        // WHEN
+        ResponseEntity<Object> response = restTemplate.postForEntity("http://localhost:" + port + "/api/v1/customer", requestEntity, Object.class);
+
+        // THEN
+        Map<String, String> errors = (Map<String, String>) response.getBody();
+
+        assertEquals(400, response.getStatusCode().value());
+        assertTrue(errors.keySet().contains("customerTvaNumber"));
+    }
+
+    @Test
+    void ItShouldDisplayAnErrorWhenEmailAlreadyExists() {
+
+        // GIVEN
+        Customer customer = Customer.builder()
+                .customerEmail("sephora@gmail.com")
+                .customerName("Sephora")
+                .customerAddress("france")
+                .customerPhone("0123456789")
+                .customerTvaNumber("FR01234567890")
+                .build();
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Customer> requestEntity = new HttpEntity<>(customer, headers);
+        restTemplate.postForEntity("http://localhost:" + port + "/api/v1/customer", requestEntity, Object.class);
+
+
+        // WHEN
+        ResponseEntity<Object> response = restTemplate.postForEntity("http://localhost:" + port + "/api/v1/customer", requestEntity, Object.class);
+
+        // THEN
+        Map<String, String> errors = (Map<String, String>) response.getBody();
+
+        assertEquals(400, response.getStatusCode().value());
+        assertTrue(errors.keySet().contains("customerEmail"));
+        assertTrue(errors.get("customerEmail").equals("l'email du client existe déjà"));
     }
 
 }
