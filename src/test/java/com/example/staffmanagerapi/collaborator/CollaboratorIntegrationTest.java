f13b03008604722cc9a9274ade9374b5e6caf3e7
tarik <tbenmaddi@gmail.com>
2023-07-22 18:26:41 +0100

    getAllCollaborator + integration Test

--- src/test/java/com/example/staffmanagerapi/collaborator/CollaboratorIntegrationTest.java
+++ src/test/java/com/example/staffmanagerapi/collaborator/CollaboratorIntegrationTest.java
@@ -4,6 +4,7 @@ package com.example.staffmanagerapi.collaborator;
 import com.example.staffmanagerapi.model.Collaborator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,11 +12,15 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class CollaboratorIntegrationTest {
 
-
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
 
     ResponseEntity<String> lastResponse;
 
@@ -44,6 +49,27 @@ class CollaboratorIntegrationTest {
 
         //Then
         assertEquals(HttpStatusCode.valueOf(201),lastResponse.getStatusCode());
+    }
+
+    @Test
+    void shouldGetAllCollaborators() {
+        // Given
+        HttpEntity<Void> requestEntity = new HttpEntity<>(null);
+
+        // When
+        ResponseEntity<Collaborator[]> responseEntity = restTemplate.exchange(
+                "http://localhost:"+port+"/api/v1/collaborator",
+                HttpMethod.GET,
+                requestEntity,
+                Collaborator[].class
+        );
+
+        // Then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+
+
+        List<Collaborator> collaborators = Arrays.asList(responseEntity.getBody());
+
     }
 
 
