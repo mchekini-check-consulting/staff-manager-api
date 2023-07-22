d7a9f16fb7e1ac93f5c2ad7a29c3e4a1ce245ac5
tarik <tbenmaddi@gmail.com>
2023-07-22 18:26:41 +0100

    getAllCollaborator + integration Test

--- src/main/java/com/example/staffmanagerapi/resource/CollaboratorResource.java
+++ src/main/java/com/example/staffmanagerapi/resource/CollaboratorResource.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/collaborator")
@@ -23,6 +22,13 @@ public class CollaboratorResource {
     @Autowired
     public CollaboratorResource(CollaboratorService collaboratorService) {
         this.collaboratorService = collaboratorService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Collaborator>> getAllCollaborators() {
+        List<Collaborator> collaborators = collaboratorService.getAllCollaborators();
+        return ResponseEntity.ok(collaborators);
     }
 
     @PostMapping("")
