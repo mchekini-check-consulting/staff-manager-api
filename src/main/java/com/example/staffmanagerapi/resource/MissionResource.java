3b2d4e3f67ef2f61bf53cc6043ded19e626405ee
sskarim85@gmail.com <sskarim85@gmail.com>
2023-07-22 12:15:05 +0100

    added get all missions endpoint

--- src/main/java/com/example/staffmanagerapi/resource/MissionResource.java
+++ src/main/java/com/example/staffmanagerapi/resource/MissionResource.java
@@ -1,17 +1,18 @@
 package com.example.staffmanagerapi.resource;
 
 import com.example.staffmanagerapi.dto.mission.in.CreateMissionInDto;
+import com.example.staffmanagerapi.dto.mission.in.MissionDto;
+import com.example.staffmanagerapi.model.Mission;
 import com.example.staffmanagerapi.service.MissionService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/mission")
@@ -34,5 +35,12 @@ public class MissionResource {
                 .buildAndExpand(missionId)
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping()
+    public ResponseEntity getAllMissions(){
+        List<MissionDto> missions = missionService.missions();
+        return ResponseEntity.status(HttpStatusCode.valueOf(200))
+                .body(missions);
     }
 }
