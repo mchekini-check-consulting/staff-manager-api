f18563193a192bc524c62e5a59ca832ad0365cf2
mahdi chekini <mchekini@checkconsulting.fr>
2023-07-26 01:34:16 +0200

    disable tests on DocumentIntegrationTest

--- src/test/resources/testDocument.sql
+++ src/test/resources/testDocument.sql
@@ -1,9 +1,12 @@
 -- Insert three collaborators
+
+ALTER TABLE collaborator ALTER COLUMN id RESTART WITH 1;
+
 INSERT INTO collaborator (firstName, lastName, address, email, phone)
 VALUES
     ('John', 'Doe', 'New York City', 'john.doe@example.com', '1234567890'),
     ('Jane', 'Smith', 'Los Angeles', 'jane.smith@example.com', '9876543210'),
-    ('Michael', 'Johnson', 'Chicago', 'michael.johnson@example.com', '5555555555');
+    ( 'Michael', 'Johnson', 'Chicago', 'michael.johnson@example.com', '5555555555');
 
 -- Insert two documents for each collaborator
 INSERT INTO document (name, type, collaborator_id, created_at)
