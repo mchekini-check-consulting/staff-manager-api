5ca6d3871250c95d872f3fef8430ac0a31a847b1
samlogy <aceportgasd235@gmail.com>
2023-07-25 21:59:43 +0100

    SM-63 dépot de piéces justificatives

--- src/main/java/com/example/staffmanagerapi/exception/FileEmptyException.java
+++ src/main/java/com/example/staffmanagerapi/exception/FileEmptyException.java
@@ -1,9 +1,5 @@
 package com.example.staffmanagerapi.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-
 public class FileEmptyException extends RuntimeException {
     public FileEmptyException(String message) {
         super(message);
