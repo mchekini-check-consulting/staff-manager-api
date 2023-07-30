50bc7231851c8c0474f391dedbfc89f9b00c8cf3
Abdallah Hameur lain <abdallah.hameurlain@eadn.dz>
2023-07-30 02:54:07 +0100

    feat : implement Jasper Reports v6.20.1

--- src/main/java/com/example/staffmanagerapi/resource/ActivityResource.java
+++ src/main/java/com/example/staffmanagerapi/resource/ActivityResource.java
@@ -5,30 +5,57 @@ import com.example.staffmanagerapi.dto.activity.in.CreateActivityInDto;
 import com.example.staffmanagerapi.dto.activity.out.CompteRenduActiviteOutDto;
 import com.example.staffmanagerapi.model.User;
 import com.example.staffmanagerapi.service.ActivityService;
+import com.example.staffmanagerapi.service.JasperReportService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.YearMonth;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/activity")
+@Slf4j
 public class ActivityResource {
 
-
     private final ActivityService activityService;
+    JasperReportService jasperReportService;
     private final User user;
 
-    public ActivityResource(ActivityService activityService, User user) {
+    public ActivityResource(ActivityService activityService, User user,JasperReportService jasperReportService) {
         this.activityService = activityService;
         this.user = user;
+        this.jasperReportService = jasperReportService;
     }
 
     @GetMapping
     @Authenticated(authenticated = true, hasAnyRoles = {"admin"})
     public ResponseEntity<List<CompteRenduActiviteOutDto>> getCurrentMonthCRA() {
         return ResponseEntity.ok(this.activityService.getCurrentMonthCRA());
+    }
+
+    @GetMapping(value = "/report", produces = MediaType.APPLICATION_PDF_VALUE)
+    //@Authenticated(authenticated = true, hasAnyRoles = {"admin"})
+    public <T> ResponseEntity<byte[]> generatePDFReport() {
+        try {
+            List<CompteRenduActiviteOutDto> data = this.activityService.getCurrentMonthCRA();
+            byte[] pdfBytes = jasperReportService.generateReport("reports/cra.jrxml", data);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", "compte-rendu-activité-"+ YearMonth.now() +".pdf");
+
+            // Return the PDF in the response body
+            return ResponseEntity.ok().headers(headers).body(pdfBytes);
+        } catch (Exception e) {
+            // Handle exceptions, return an error response, or log the error
+            log.error("Une erreur est survenues lors de la generation du report des comptes-rendu-d'activite en PDF !");
+            return ResponseEntity.status(500).body(null);
+        }
     }
 
     @PostMapping
---
+++ src/main/java/com/example/staffmanagerapi/service/JasperReportService.java
@@ -0,0 +1,82 @@
+package com.example.staffmanagerapi.service;
+
+import lombok.extern.slf4j.Slf4j;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class JasperReportService {
+
+    ActivityService activityService;
+
+    @Autowired
+    public JasperReportService(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    public <T> byte[] generateReport(String reportClassPathLocation, List<T> data) {
+
+        try {
+            log.info("Tentative de géneration du rapport {} contenant {} lignes ... ", reportClassPathLocation, data.size());
+            // Load the Jasper Report design file
+            Resource reportResource = new ClassPathResource(reportClassPathLocation); // example : "reports/cra.jrxml"
+            InputStream reportInputStream = reportResource.getInputStream();
+            // Set custom font directory (the folder where you placed the Roboto font files)
+            String customFontDirectory = "fonts/";
+            // Compile the Jasper Report with the custom font
+            JasperReport jasperReport = JasperCompileManager.compileReport(reportInputStream);
+
+            // Data source, comes from a service or repository ... or mock data
+            JRDataSource dataSource = new JRBeanCollectionDataSource(data);
+
+            Map<String, Object> parameters = generateReportParameters();
+            // Generate the report
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+            // Export report as PDF
+            byte[] pdf = JasperExportManager.exportReportToPdf(jasperPrint);
+            log.info("Rapport qui existe dans {} est généré avec success !",reportClassPathLocation);
+            return pdf;
+        } catch (IOException e) {
+            log.error("Erreur lors de la generation du rapport, causes possibles : Image introuvable dans classpath, fichier {}.jrxml introuvable ", reportClassPathLocation);
+            log.error("Erreur lors de la génération du rapport. Caused by: {}", e.getMessage(), e);
+        } catch (JRException e) {
+            log.error("Erreur lors de la generation du rapport, causes possibles : compilation du rapport, creation de datasource du rapport ou conversion en PDF du rapport");
+            log.error("Erreur lors de la génération du rapport. Caused by: {}", e.getMessage(), e);
+        }
+        throw new RuntimeException();
+    }
+
+    public Map<String, Object> generateReportParameters() throws IOException {
+        // Fetch the logo from the classpath
+        Resource imageResource = new ClassPathResource("reports/assets/logo.png");
+        InputStream imageInputStream = imageResource.getInputStream();
+
+        // Create parameters map and add the image as a parameter
+        // + other params
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("logo", ImageIO.read(imageInputStream));
+        parameters.put("company-name", "Check Consulting"); // j'ai écris sa en dur, mais on va le remplacer par les données en base.
+        parameters.put("company-tva", 19);
+        parameters.put("company-contact-name", "Chekini");
+        parameters.put("company-contact-email", "mchekini@checkconsulting.fr");
+        parameters.put("company-adress", "16 rue de la Voute, Paris 75012");
+        parameters.put("company-capital", "12345 £");
+        parameters.put("company-siret", "123 568 941 00056");
+
+        return parameters;
+    }
+
+}
