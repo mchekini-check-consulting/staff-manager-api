7b25816fad8ac840424bff74d80aab1b733d129a
mahdi chekini <mchekini@checkconsulting.fr>
2023-07-31 02:55:33 +0200

    Update integration configuration

--- src/main/java/com/example/staffmanagerapi/configuration/AwsConfiguration.java
+++ src/main/java/com/example/staffmanagerapi/configuration/AwsConfiguration.java
@@ -1,34 +1,26 @@
 package com.example.staffmanagerapi.configuration;
 
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class AwsConfiguration {
 
-    @Value("${aws.access-key-id}")
-    private String accessKey;
-
-    @Value("${aws.secret-access.key}")
-    private String secretKey;
 
     @Value("${aws.region}")
     private String region;
 
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
