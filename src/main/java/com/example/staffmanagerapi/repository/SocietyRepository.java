12545963098e08088b6f22e33d9215d3e72ad312
xlincw0w <khaled.khazem@yahoo.com>
2023-08-03 19:25:01 +0100

    feat/SM-304: create or update society

---
+++ src/main/java/com/example/staffmanagerapi/repository/SocietyRepository.java
@@ -0,0 +1,6 @@
+package com.example.staffmanagerapi.repository;
+
+import com.example.staffmanagerapi.model.Society;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocietyRepository extends JpaRepository<Society, Integer> {}
