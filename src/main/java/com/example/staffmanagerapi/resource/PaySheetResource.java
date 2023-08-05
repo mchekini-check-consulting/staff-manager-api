a15676170c2525238ab0e2d72260db2cee5444cd
HL-Abdallah <68193549+HL-Abdallah@users.noreply.github.com>
2023-08-05 20:49:48 +0100

    FEAT : Paysheet search (#31)
    Co-authored-by: Abdallah Hameur lain <abdallah.hameurlain@eadn.dz>

--- src/main/java/com/example/staffmanagerapi/resource/PaySheetResource.java
+++ src/main/java/com/example/staffmanagerapi/resource/PaySheetResource.java
@@ -1,27 +1,49 @@
 package com.example.staffmanagerapi.resource;
 
 import com.example.staffmanagerapi.dto.paysheet.CreatePaySheetDTO;
+import com.example.staffmanagerapi.dto.paysheet.SearchPaySheetDTO;
+import com.example.staffmanagerapi.model.Paysheet;
+import com.example.staffmanagerapi.model.User;
 import com.example.staffmanagerapi.service.PaySheetService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/paysheet")
+@Slf4j
 public class PaySheetResource {
 
     private final PaySheetService paySheetService;
+    private User user;
 
-    public PaySheetResource(PaySheetService paySheetService) {
+    public PaySheetResource(PaySheetService paySheetService, User user) {
         this.paySheetService = paySheetService;
+        this.user = user;
     }
 
     @PostMapping()
     public ResponseEntity<?> upload(@ModelAttribute @Valid CreatePaySheetDTO paysheet) throws IOException {
         Integer created = paySheetService.uploadFile(paysheet);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> recherche(@RequestBody @Valid SearchPaySheetDTO searchPaySheetDTO) throws IOException {
+        try {
+            log.info("Tentative de recherche des fiches de paie du collaborateur {} ... ", user.getEmail());
+            List<Paysheet> response = paySheetService.search(searchPaySheetDTO, user);
+            log.info("Recherche des fiches de paie du collaborateur {} effectué avec success, n°fiches : {} ", user.getEmail(), response.size());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            log.error("Erreur lors de la recherches des fiches de paie de l'utilisateur {}", user.getEmail());
+            log.error("{}", e.getMessage());
+            throw new RuntimeException();
+        }
     }
 }
--- src/main/java/com/example/staffmanagerapi/service/PaySheetService.java
+++ src/main/java/com/example/staffmanagerapi/service/PaySheetService.java
@@ -1,31 +1,37 @@
 package com.example.staffmanagerapi.service;
 
 import com.example.staffmanagerapi.dto.paysheet.CreatePaySheetDTO;
+import com.example.staffmanagerapi.dto.paysheet.SearchPaySheetDTO;
 import com.example.staffmanagerapi.exception.FileEmptyException;
 import com.example.staffmanagerapi.exception.FileInvalidExtensionException;
 import com.example.staffmanagerapi.exception.FileNameExistsException;
 import com.example.staffmanagerapi.model.Collaborator;
 import com.example.staffmanagerapi.model.Paysheet;
+import com.example.staffmanagerapi.model.User;
 import com.example.staffmanagerapi.repository.CollaboratorRepository;
 import com.example.staffmanagerapi.repository.PaySheetRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 
 @Service
 public class PaySheetService {
 
-    @Value("${bucket.fiche-de-paie}")
-    private String paysheetBucket;
     private final PaySheetRepository paySheetRepository;
     private final CollaboratorRepository collaboratorRepository;
     private final AmazonS3Service amazonS3Service;
+    @Value("${bucket.fiche-de-paie}")
+    private String paysheetBucket;
 
     public PaySheetService(PaySheetRepository paySheetRepository, CollaboratorRepository collaboratorRepository, AmazonS3Service amazonS3Service) {
         this.paySheetRepository = paySheetRepository;
@@ -35,15 +41,15 @@ public class PaySheetService {
 
     public Integer uploadFile(CreatePaySheetDTO paySheetDTO) throws FileEmptyException, EntityNotFoundException, FileInvalidExtensionException, FileNameExistsException, IOException {
         Collaborator collaborator = collaboratorRepository.findById(paySheetDTO.getCollaborator().longValue())
-                .orElseThrow(()-> new EntityNotFoundException("Ce collaborateur n'existe pas"));
+                .orElseThrow(() -> new EntityNotFoundException("Ce collaborateur n'existe pas"));
 
-        if(paySheetDTO.getFile().isEmpty()){
+        if (paySheetDTO.getFile().isEmpty()) {
             throw new FileEmptyException("Veuillez séléctionner un fichier valide non vide");
         }
 
         boolean isAttributed = paySheetRepository.existsByCollaboratorAndMonthYear(collaborator, paySheetDTO.getMonthYear());
 
-        if(isAttributed){
+        if (isAttributed) {
             throw new FileNameExistsException("Fiche de paie déjà attribuée");
         }
 
@@ -79,4 +85,49 @@ public class PaySheetService {
         if (Objects.isNull(multipartFile.getOriginalFilename())) return false;
         return !multipartFile.getOriginalFilename().trim().equals("");
     }
+
+    public List<Paysheet> search(SearchPaySheetDTO searchPaySheetDTO, User user) {
+
+        Optional<Collaborator> collaboratorOptional = this.collaboratorRepository.findByEmail(user.getEmail());
+        if (collaboratorOptional.isEmpty()) {
+            throw new EntityNotFoundException("Le Collaborateur qui possede l'email " + user.getEmail() + " n'existe pas!");
+        }
+        Collaborator collaborator = collaboratorOptional.get();
+
+        Specification<Paysheet> specification = Specification
+                .where(this.filterByStartInterval(searchPaySheetDTO))
+                .and(this.filterByEndInterval(searchPaySheetDTO))
+                .and(this.filterByCollaborator(collaborator));
+
+        Sort sortOptions = Sort.by(Sort.Direction.DESC, "monthYear");
+        return this.paySheetRepository.findAll(specification, sortOptions);
+    }
+
+    private Specification<Paysheet> filterByStartInterval(SearchPaySheetDTO searchPaySheetDTO) {
+        if (searchPaySheetDTO.getStartDate() != null) {
+            return (root, query, criteriaBuilder) -> {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("monthYear"), searchPaySheetDTO.getStartDate());
+            };
+        }
+        ;
+        return null;
+    }
+
+    private Specification<Paysheet> filterByEndInterval(SearchPaySheetDTO searchPaySheetDTO) {
+        if (searchPaySheetDTO.getEndDate() != null) {
+            return (root, query, criteriaBuilder) -> {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("monthYear"), searchPaySheetDTO.getEndDate());
+            };
+        }
+        ;
+        return null;
+    }
+
+    private Specification<Paysheet> filterByCollaborator(Collaborator collaborator) {
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.equal(root.get("collaborator"), collaborator);
+        };
+    }
+
+
 }
