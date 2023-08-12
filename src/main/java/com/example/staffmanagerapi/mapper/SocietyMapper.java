21944b24e6b59385811cbbb1cbb756b2023401f8
mahdi chekini <mchekini@checkconsulting.fr>
2023-08-12 18:41:49 +0200

    Add Get Society Informations Feature

--- src/main/java/com/example/staffmanagerapi/mapper/SocietyMapper.java
+++ src/main/java/com/example/staffmanagerapi/mapper/SocietyMapper.java
@@ -3,7 +3,9 @@ package com.example.staffmanagerapi.mapper;
 import com.example.staffmanagerapi.dto.society.SocietyDto;
 import com.example.staffmanagerapi.model.Society;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface SocietyMapper {
 
